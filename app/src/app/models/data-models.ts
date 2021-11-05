9918db9228dedb8c629223d63e403ffe316deb5a
Reena Zhang <reena.zhang22@gmail.com>
2021-11-05 15:51:54 -0500

    update data models for front-end

--- app/src/app/models/data-models.ts
+++ app/src/app/models/data-models.ts
@@ -1,6 +1,35 @@
 // TODO fix this
-export interface MyProject {
-  _id: any;
-  title: string;
-  startDate: Date;
+export class MyProject {
+  constructor(
+    public _id: number,
+    public pname: string,
+    public powner: string,
+    public ptype: string,
+    public pstartDate: Date,
+    public checkPoints: Array<Checkpoint>,
+    public pdescription: string
+  ) {  }
+}
+
+export class Project {
+
+  constructor(
+    public _id: number,
+    public pname: string,
+    public powner: string,
+    public ptype: string,
+    public pstartDate: Date,
+    public checkPoints: Array<Checkpoint>,
+    public pdescription: string
+  ) {  }
+
+}
+
+export class Checkpoint {
+
+  constructor (
+    public pname: string,
+    public cname: string,
+    public cdate: Date
+  ) { }
 }
