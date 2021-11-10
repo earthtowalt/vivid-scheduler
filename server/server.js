9b26fd4a4fd8991f84ff526889eb1a9eb31fd05c
Alice Qiao <alice.qiao@vanderbilt.edu>
2021-11-10 16:16:08 -0600

    Remove unneeded code

--- server/server.js
+++ server/server.js
@@ -34,40 +34,6 @@ app.set("port", port);
 
 const server = http.createServer(app);
 
-// data models
-// const Project = require("./models/project");
-
-// app.get("/", (req, res) => res.send("Welcome!"));
-
-// app.get("/api/projects", function (req, res) {
-//   Project.find(function (err, projects) {
-//     if (err) res.send(err);
-//     console.log(projects);
-//     res.json(projects); // return all projects in JSON format
-//   });
-// });
-
-
-// app.post("/api/projects", function (req, res) {
-//   let data = req.body;
-//   let project = new Project();
-
-//   project.title = data.pname
-//   project.owner = data.powner
-//   project.type = data.ptype
-//   project.checkPoints = data.checkPoints
-//   project.startDate = data.startDate
-//   project.description = data.description
-  
-
-//   project.save(function (err) {
-//     if (err) {
-//       res.send(err);
-//     }
-//     res.sendStatus(200);
-//   });
-// });
-
 server.listen(port, function () {
   console.info(`Server is up and running on port ${port}`);
 });
