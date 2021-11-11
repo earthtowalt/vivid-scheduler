651b7dba8b6e1d4cddc0113c3a6ac7f719285029
earthtowalt <jackwalton00@aol.com>
2021-11-11 06:21:42 -0600

    finished this round of server tests

--- server/config/database.config.js
+++ server/config/database.config.js
@@ -1,3 +1,30 @@
+
+let MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
+let Project = require('../models/project');
+let env = require('cross-env');
+
+let mongoServer = -1;
+
 module.exports = {
-    url: 'mongodb://localhost:27017/vivid-scheduler'
-}
+  getUrl: async() => {
+  if (env.NODE_ENV === 'prod') {
+    return 'mongodb://localhost:27017/vivid-scheduler';
+  }
+
+  if (mongoServer != -1) {
+    return mongoServer.getUri();
+  }
+
+  mongoServer = await MongoMemoryServer.create();
+
+  // populate test data
+  Project.insertMany([
+    {pname: 'Project A', powner: 'Jack'}, 
+    {pname: 'Project B', powner: 'Alice'}, 
+    {pname: 'Project C', powner: 'Reena'}, 
+    {pname: 'Project D', powner: 'Sophia'}
+  ]);
+
+  return mongoServer.getUri();
+
+}};
--- server/tests/project-test.js
+++ server/tests/project-test.js
@@ -1,116 +1,149 @@
 "use strict";
 
-const should = require("should");
-const assert = require("assert");
-const request = require("superagent");
-// const harness = require("./harness");
-const data = require("./data");
-// let config = {};
-let projects = data.projects;
-const envConfig = require("simple-env-config");
-const env = process.env.NODE_ENV ? process.env.NODE_ENV : "test";
+// const should = require("should");
+// const assert = require("assert");
+// // const harness = require("./harness");
+// const data = require("./data");
+// // let config = {};
+// let projects = data.projects;
+// // const envConfig = require("simple-env-config");
+// const env = process.env.NODE_ENV ? process.env.NODE_ENV : "test";
 
-let config = {
-  url: `http://localhost:8000/api/`,
-};
+// let config = {
+//   url: `http://localhost:8000/api/`,
+// };
 
-describe("Project:", () => {
-  let primaryAgent = request.agent(),
-    anonAgent = request.agent();
-  before((done) => {
-    // envConfig("./config/config.json", env).then((conf) => {
-    //   config = conf;
-    //   config.url = `http://localhost:8000/api/`;
-    //   harness.setup(config.mongodb, done);
+const request = require('supertest');
+const app = require('../server');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+let dbConfig = require('../config/database.config');
+const Project = require('../models/project');
+
+describe('Project (POST) Endpoint', () => {
+
+  before(() => {
+    mongoose.connect(dbConfig.getUrl());
+  });
+
+  after(() => {
+    mongoose.disconnect();
+  })
+
+  beforeEach(() => {
+    // mongoose.Collection.drop();
+  })
+  
+  it('should create a new post with proper params', async () => {
+
+    const pname = 'New Name';
+    const res = await request(app)
+      .post('/api/project')
+      .send({
+        pname: pname,
+        powner: 'New Owner',
+        startDate: new Date()
+      });
+    expect(res.statusCode).to.equal(201);
+    expect(res.body).to.contain({pname: pname});
+  });
+
+  it("should error on missing title", async() => {
+    const res = await request(app)
+      .post(`/api/project`)
+      .send({
+        powner: 'test owner',
+        ptype: 'type',
+        startDate: new Date(),
+        description: 'description',
+        completed: 'yes',
+        url: '',
+      });
+      expect(res.statusCode).to.equal(400);
+    });
+
+  it("should error on missing start date", async () => {
+    const res = await request(app)
+      .post(`/api/project`)
+      .send({
+        pname: 'Test Name',
+        powner: 'test_owner',
+        ptype: 'type',
+        description: 'description',
+        completed: 'yes',
+        url: '',
+      });
+      expect(res.statusCode).to.equal(400);
+  });
+
+  // it("second exapmle where post is correct???", (done) => {
+  //   primaryAgent
+  //     .post(`${config.url}project`)
+  //     .send(projects.primary)
+  //     .end((err, res) => {
+  //       res.status.should.equal(201);
+  //       res.body.pname.should.equal(projects.primary.pname);
+  //       // Save the project info
+  //       setTimeout(done, 100);
+  //     });
+  // });
+
+  it("should error on repeated pname", async () => {
+    const res = await request(app)
+      .post(`/api/project`)
+      .send({
+        pname: 'Name A',
+        startDate: new Date()
+    });
+    expect(res.statusCode).to.equal(201);
+    const badRes = await request(app)
+      .post(`/api/project`)
+      .send({
+        pname: 'Name A',
+        startDate: new Date()
+    });
+    expect(badRes.statusCode).to.equal(400);
+  });
+});
+
+
+describe("Projects (GET) Endpoint", () => {
+
+  before(async () => {
+    let url = await dbConfig.getUrl();
+    // console.log(url);
+    mongoose.connect(url);
+  });
+
+  after(() => {
+    mongoose.disconnect();
+  });
+
+  it("should return empty list when DB is empty", async () => {
+    const res = await request(app).get('/api/projects').expect(200);
+    // console.log(res.body);
+    expect(res.body).to.deep.equal([]);
+  });
+
+  it("should return correct response when entry exists in db", async() => {
+    // @@TODO test all fields
+    Project.create({pname: "Tester G", startDate: new Date()});
+    Project.create({pname: "Tester H", startDate: new Date()});
+
+    const res = await request(app).get('/api/projects');
+
+    expect(res.status).to.equal(200);
+    expect(res.body.length).to.equal(2);
+    
+    // primaryAgent.get("http://localhost:8000/api/projects").end((err, res) => {
+    //   res.status.should.equal(200);
+    //   res.body.pname.should.equal(users.primary.pname);
+    //   res.body.powner.should.equal(users.primary.powner);
+    //   res.body.ptype.should.equal(users.primary.ptype);
+    //   res.body.checkpoints.should.be.instanceof(Array).and.have.lengthOf(5);
+    //   res.body.description.should.equal(users.primary.description);
+    //   res.body.completed.should.equal(users.primary.completed);
+    //   res.body.url.should.equal(users.primary.url);
     // });
-  });
-  after((done) => {
-    // harness.shutdown(done);
-  });
-
-  describe("Read empty:", () => {
-    it("Success - empty", (done) => {
-      primaryAgent.get(`${config.url}projects`).end((err, res) => {
-        res.status.should.equal(200);
-        res.body.should.equal([]);
-        done();
-      });
-    });
-  });
-
-  describe("Create:", () => {
-    it("Failure - missing required title", (done) => {
-      primaryAgent
-        .post(`${config.url}project`)
-        .send({
-          powner: projects.primary.powner,
-          ptype: projects.primary.ptype,
-          startDate: projects.primary.startDate,
-          description: projects.primary.description,
-          completed: projects.primary.completed,
-          url: projects.primary.url,
-        })
-        .end((err, res) => {
-          res.status.should.equal(400);
-          res.body.error.should.equal('"pname" is required');
-          done();
-        });
-    });
-
-    it("Failure - missing required start date", (done) => {
-      primaryAgent
-        .post(`${config.url}project`)
-        .send({
-          pname: projects.primary.pname,
-          powner: projects.primary.powner,
-          ptype: projects.primary.ptype,
-          description: projects.primary.description,
-          completed: projects.primary.completed,
-          url: projects.primary.url,
-        })
-        .end((err, res) => {
-          res.status.should.equal(400);
-          res.body.error.should.equal('"startDate" is required');
-          done();
-        });
-    });
-
-    it("Success", (done) => {
-      primaryAgent
-        .post(`${config.url}project`)
-        .send(projects.primary)
-        .end((err, res) => {
-          res.status.should.equal(201);
-          res.body.pname.should.equal(projects.primary.pname);
-          // Save the project info
-          setTimeout(done, 100);
-        });
-    });
-
-    it("Failure - already used pname", (done) => {
-      primaryAgent
-        .post(`${config.url}projects`)
-        .send(projects.primary)
-        .end((err, res) => {
-          res.status.should.equal(400);
-          done();
-        });
-    });
-  });
-
-  describe("Read:", () => {
-    it("Success - empty", (done) => {
-      primaryAgent.get("http://localhost:8000/api/projects").end((err, res) => {
-        res.status.should.equal(200);
-        res.body.pname.should.equal(users.primary.pname);
-        res.body.powner.should.equal(users.primary.powner);
-        res.body.ptype.should.equal(users.primary.ptype);
-        res.body.checkpoints.should.be.instanceof(Array).and.have.lengthOf(5);
-        res.body.description.should.equal(users.primary.description);
-        res.body.completed.should.equal(users.primary.completed);
-        res.body.url.should.equal(users.primary.url);
-        done();
-      });
-    });
   });
 });
