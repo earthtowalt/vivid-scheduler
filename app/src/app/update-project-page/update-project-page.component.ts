ce74edf2702448fea30849a4d1dbbe8a1dcc3db7
earthtowalt <jackwalton00@aol.com>
2021-12-02 01:29:46 -0600

    populating dropdown list

--- app/src/app/update-project-page/update-project-page.component.ts
+++ app/src/app/update-project-page/update-project-page.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Checkpoint, Project } from '../models/data-models';
+import { ProjectService } from '../services/project.service';
+
+
 
 @Component({
   selector: 'app-update-project-page',
   templateUrl: './update-project-page.component.html',
   styleUrls: ['./update-project-page.component.css']
 })
+
 export class UpdateProjectPageComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[];
+
+  selectedProject: Project;
+
+  constructor(private _ProjectService: ProjectService) { }
 
   ngOnInit(): void {
+    //this._ProjectService.getProjects();
+    this.bindDropdown();
   }
+
+  bindDropdown() {
+    this._ProjectService.getProjects().subscribe(
+      response => {
+        this.projects = response;
+        console.log(response);
+        
+      }
+    );
+  }
+
 
 }
