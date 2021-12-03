818e827526f5bd06c7e5f83b4f30f81e919649fd
earthtowalt <jackwalton00@aol.com>
2021-12-03 15:23:30 -0600

    update project page modal created

--- app/src/app/update-project-page/update-project-page.component.ts
+++ app/src/app/update-project-page/update-project-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Checkpoint, Project } from '../models/data-models';
 import { ProjectService } from '../services/project.service';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { Observable } from 'rxjs';
 
 
 
@@ -12,26 +15,45 @@ import { ProjectService } from '../services/project.service';
 
 export class UpdateProjectPageComponent implements OnInit {
 
-  projects: Project[];
+  submitted: boolean;
+
+  projects: Observable<Project[]>;
 
   selectedProject: Project;
 
-  constructor(private _ProjectService: ProjectService) { }
+  // available project types
+  types = ['Instagram Reel', 'Youtube Video', 'TikTok', 'Other'];
+
+  constructor(private _ProjectService: ProjectService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     //this._ProjectService.getProjects();
-    this.bindDropdown();
+    this.bindSelectProjectDropdown();
   }
 
-  bindDropdown() {
-    this._ProjectService.getProjects().subscribe(
-      response => {
-        this.projects = response;
-        console.log(response);
-        
+  // bind the select project dropdown
+  bindSelectProjectDropdown() {
+    this.projects = this._ProjectService.getProjects();
+  }
+
+  // when selection is made, pop up 
+
+  open(content: any, selectedProject: Project) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-update-title'}).result.then((result) => {
+      if (result === 'save') {
+
+        alert('project sucessfully updated.');
       }
-    );
+    }, (reason) => {
+      // if the modal is closed unexpectedly
+    });
   }
 
+  // onSubmit to 
+  onSubmit(data: any) {
+    this.submitted = true;
+    console.log("submitted: " + JSON.stringify(data));
+    alert("submitted: " + JSON.stringify(data));
+  }
 
 }
