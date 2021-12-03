ee10641f7f2cd7347c550ae4cbf39ea57960415d
Alice Qiao <alice.qiao@vanderbilt.edu>
2021-12-03 13:59:29 -0600

    Create backend login endpoint

--- server/routes/base.route.js
+++ server/routes/base.route.js
@@ -40,7 +40,7 @@ const createCheckpoints = (startDate) => {
 
 // Create a new project
 router.post("/project", async (req, res) => {
-  console.log("beggining of post")
+  console.log("beggining of post");
   // Schema for project info validation
   const schema = Joi.object({
     pname: Joi.string().required(),
@@ -49,8 +49,8 @@ router.post("/project", async (req, res) => {
     startDate: Joi.date().required(),
     checkPoints: Joi.array(),
     description: Joi.string(),
-    completed: Joi.string(), 
-    url: Joi.string()
+    completed: Joi.string(),
+    url: Joi.string(),
   });
   try {
     let data = await schema.validateAsync(req.body);
@@ -66,6 +66,25 @@ router.post("/project", async (req, res) => {
       console.log(err.message);
       res.status(400).send({ error: err.message });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ error: err.details[0].message });
+  }
+});
+
+// Create a new admin
+router.post("/login", async (req, res) => {
+  // Schema for admin info validation
+  const schema = Joi.object({
+    password: Joi.string().required(),
+  });
+  try {
+    let data = await schema.validateAsync(req.body);
+    console.log(data);
+    if (data.password === "p") {
+      return res.status(201).send({ user: "admin" });
+    }
+    return res.status(400).send({ error: "Password is incorrect" });
   } catch (err) {
     console.log(err);
     res.status(400).send({ error: err.details[0].message });
